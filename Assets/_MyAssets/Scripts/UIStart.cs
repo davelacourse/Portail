373acfaf25511259f46dc8d445c82f72d101a7a2
ProfDave <dave.lacourse@cegeptr.qc.ca>
2024-11-26 16:54:19 -0500

    sd

--- Assets/_MyAssets/Scripts/UIStart.cs
+++ Assets/_MyAssets/Scripts/UIStart.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIStart : MonoBehaviour
 {
+    [SerializeField] Button _boutonSelectedDepart = default(Button);
+
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_boutonSelectedDepart.gameObject);
+    }
 
     public void StartSpaceShooter()
     {
-        Debug.Log("Chemin : " + Application.dataPath);
         System.Diagnostics.Process.Start(Application.dataPath + "/../../SpaceShooter/SpaceShooter.exe");
+        Application.Quit();
     }
 
-    public void StartGobelinicide()
+    public void StartPolygo()
     {
-        System.Diagnostics.Process.Start(Application.dataPath + "/../../Gobelinicide/Gobelinicide.exe");
+        System.Diagnostics.Process.Start(Application.dataPath + "/../../Polygo/Polygo.exe");
+        Application.Quit();
+    }
+
+    public void StartMysticArena()
+    {
+        System.Diagnostics.Process.Start(Application.dataPath + "/../../MysticArena/MysticArena.exe");
+        Application.Quit();
     }
 }
